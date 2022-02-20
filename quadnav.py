46cf51956816a4a906ae5fb694d4c180edb805c1
gilmorc5 <79158319+gilmorc5@users.noreply.github.com>
2022-02-19 23:08:35 -0500

    Commit - Error in * and /

--- quadnav.py
+++ quadnav.py
@@ -2,6 +2,8 @@
 # Charles Gilmore
 # Feb. 19, 2022
 #
+# Project #2 - Quad Copter Navigation
+#
 # A quadcopter is located at latitude 29.401325 and longitude -81.177222 the quadrotor is oriented so that the y axis is aligned with north/south and the x axis is aligned with east/west. 
 # Positive accelerations on the y axis are towards the north and positive accelerations on the x axis are towards the east. Time is in seconds and the acceleration data units are cm/sec^2.
 #
@@ -13,56 +15,60 @@
 #####################################################################
 
 import openpyxl
-
+from array import *
+from math import *
 
 ## Global Defs
 # Initial Conditions
-lat_x = 29.401325   # (-) West -> (+) East X axis
-long_y = -81.177222 # (-) South -> (+) North Y axis
+lat = 29.401325   # (-) West -> (+) East X axis
+long = -81.177222 # (-) South -> (+) North Y axis
+R = 6378137 # Earth's Radius (meters)
 init_x_vel = -0.2 # m/s
 init_y_vel = 0.44 # m/s
+time = 0
 
-# Accelerometer excel sheet
+# Delcare location(path) to excel file > Workbook > Specific Sheet in WB
 path = ("quad_accel.xlsx")
 wb = openpyxl.load_workbook(path)
 sheet = wb.active
 
-# Find max rows & columns
+# Find max rows & columns of file
 max_col = sheet.max_column
 max_row = sheet.max_row
 
-# initialize a 2D array to hold the values of the excel sheet
+# Calculate Long/Lat when given X/Y Displacement
+# input: Y Displacement, Init_Long
+def findLong(long, Ydisp_m):
+    # Disp in radians
+    long_rad = Ydisp_m/(R * cos((pi * lat)/180))
+    # Offset in Longitude 
+    long = long + long_rad * (180 / pi)
+    return long
 
-# Read Time
+# Calculate Long/Lat when given X/Y Displacement
+# input: X Displacement, Init_Lat
+def findLat(lat, Xdisp_m):
+    # Disp in radians
+    lat_rad = Xdisp_m / R
+    lat = lat + lat_rad * (180 / pi)
+    return lat
 
-time = sheet.cell(row = 2, column = 1).value
-Xaccel_cm = sheet.cell(row = 2, column = 2).value
-Yaccel_cm = sheet.cell(row = 2, column = 3).value
-
-print(time)
-print(Xaccel_cm)
-print(Yaccel_cm)
-
-# X cm to meters
-def cm_to_m(accel_cm):
-    return(accel_cm/100)
-
-Xaccel_m = cm_to_m(Xaccel_cm)
-Yaccel_m = cm_to_m(Yaccel_cm)
-
-# Meters to Coordinates
-
-for i in range(1, max_col+1):
-    cell = sheet.cell(row = 1, column = i)
-    #print(cell.value)
-
-# Displacement from acceleration formula
-# Sending initial Velocity & acceleration & time
-def accToDist_X(time, accel, init_x_vel):
-    return(init_x_vel * time + 0.5 * accel * pow(time, 2))
-
-print()
-print(accToDist_X(time, Xaccel_m, init_x_vel))
+# for-in loop to read through the excel data sheet and make calculations
+# Read cell value -> convert cm to m -> calculate displacement from acceleration -> put into array
+for i in range(1, max_row+1):
+    time+=0.1
+    # Convert X acceleration (cm/s^2 > m/s^2) to displacement and put in 1D array
+    # Displacement from acceleration formula
+    # displ = (Vi)(t) + 1/2(accel)(t^2)
+    # Sending initial Velocity, acceleration, time
+    Xaccel_m = (sheet.cell(row=i+1, column=2).value)
+    Xdisp_m = (init_x_vel * time) + (0.5 * Xaccel_m * (time ** 2))
+    Xlat_array = [findLat(lat, Xdisp_m) for i in range(max_row)]
+    
+    # Convert Y acceleration (cm/s^2 > m/s^2) to displacement and put in 1D array
+    Yaccel_m = (sheet.cell(row=i+1, column=3).value)
+    Ydisp_m = (init_y_vel * time) + (0.5 * Yaccel_m * (time ** 2))
+    Ylong_array = [findLong(long, Ydisp_m) for i in range(max_row)]
 
 
 
